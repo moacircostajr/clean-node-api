c469fbcadd652a16c9c6eeee6231f47387478369
Moacir Costa <moacircostajr@gmail.com>
2020-09-24 16:43:59 -0300

    test: ensure SignUpController return an error if no email is provided

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -4,13 +4,30 @@ describe('SignUp Controller', () => {
   test('Should return 400 if no name is provided', () => {
     const sut = new SignUpController() // system under test
     const httpRequest = {
-      // name: 'any_name',
-      email: 'any_email@mail.com.br',
-      password: 'any_password',
-      passwordConfirmation: 'any_password'
+      body: {
+        // name: 'any_name',
+        email: 'any_email@mail.com.br',
+        password: 'any_password',
+        passwordConfirmation: 'any_password'
+      }
     }
     const httpResponse = sut.handle(httpRequest)
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body).toEqual(new Error('Missing param: name'))
+  })
+
+  test('Should return 400 if no email is provided', () => {
+    const sut = new SignUpController() // system under test
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        // email: 'any_email@mail.com.br',
+        password: 'any_password',
+        passwordConfirmation: 'any_password'
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new Error('Missing param: email'))
   })
 })
