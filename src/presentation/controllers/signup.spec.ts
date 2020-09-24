ffb48fc9cd6f0c06a5402a5a85321d1296d0c111
Moacir Costa <moacircostajr@gmail.com>
2020-09-24 18:01:03 -0300

    refactor: simplify code

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -1,11 +1,10 @@
-import { HttpRequest } from '../protocols/http'
 import { SignUpController } from './signup'
 import { MissingParamError } from '../errors/missing-param-error'
 
 describe('SignUp Controller', () => {
   test('Should return 400 if no name is provided', () => {
     const sut = new SignUpController()
-    const httpRequest: HttpRequest = {
+    const httpRequest = {
       body: {
         name: '',
         email: 'any_email@mail.com.br',
@@ -20,10 +19,9 @@ describe('SignUp Controller', () => {
 
   test('Should return 400 if no email is provided', () => {
     const sut = new SignUpController() // system under test
-    const httpRequest: HttpRequest = {
+    const httpRequest = {
       body: {
         name: 'any_name',
-        email: '',
         password: 'any_password',
         passwordConfirmation: 'any_password'
       }
--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -3,12 +3,10 @@ import { badRequest } from '../helpers/http-helper'
 import { HttpRequest, HttpResponse } from '../protocols/http'
 
 export class SignUpController {
-  handle (httpRequest: HttpRequest): HttpResponse {
-    if (!httpRequest.body.name) {
-      return badRequest(new MissingParamError('name'))
-    }
-    if (!httpRequest.body.email) {
-      return badRequest(new MissingParamError('email'))
+  handle (httpRequest): HttpResponse {
+    const requiredFields = ['name', 'email']
+    for (const field of requiredFields) {
+      if (!httpRequest.body[field]) { return badRequest(new MissingParamError(field)) }
     }
     return {
       statusCode: 200,
