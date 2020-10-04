b6da12f64104d8c561e728372c27c623f7afab5d
Moacir Costa <moacircostajr@gmail.com>
2020-10-04 06:23:34 -0300

    feat: ensure SignUpController calls AddAccount with correct values

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,15 +1,18 @@
+import { AddAccount } from '../../domain/useCases/add-account'
 import { InvalidParamError, MissingParamError } from '../errors'
 import { badRequest, serverError } from '../helpers/http-helper'
-import { Controller, HttpResponse, EmailValidator } from '../protocols'
+import { Controller, HttpResponse, EmailValidator, HttpRequest } from '../protocols'
 
 export class SignUpController implements Controller {
   private readonly emailValidator: EmailValidator
+  private readonly addAccount: AddAccount
 
-  constructor(emailValidator: EmailValidator) {
+  constructor(emailValidator: EmailValidator, addAccount: AddAccount) {
     this.emailValidator = emailValidator
+    this.addAccount = addAccount
   }
 
-  handle(httpRequest): HttpResponse {
+  handle(httpRequest: HttpRequest): HttpResponse {
     try {
       const requiredFields = ['name', 'email', 'password', 'passwordConfirmation']
       for (const field of requiredFields) {
@@ -17,13 +20,18 @@ export class SignUpController implements Controller {
           return badRequest(new MissingParamError(field))
         }
       }
-      const { email, password, passwordConfirmation } = httpRequest.body
+      const { name, email, password, passwordConfirmation } = httpRequest.body
       if (password !== passwordConfirmation) return badRequest(new InvalidParamError('passwordConfirmation'))
       const isValidEmail: boolean = this.emailValidator.isValid(email)
       if (!isValidEmail) return badRequest(new InvalidParamError('email'))
+      const added = this.addAccount.add({
+        name,
+        email,
+        password
+      })
       return {
-        statusCode: 200,
-        body: null
+        statusCode: 201,
+        body: added
       }
     } catch (error) {
       return serverError()
