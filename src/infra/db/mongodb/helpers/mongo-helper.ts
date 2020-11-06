68710843530eef0bd47749db29e642978daf8cfa
Moacir Costa <moacircostajr@gmail.com>
2020-11-06 18:21:50 -0300

    feat: testando o mongodb

---
+++ src/infra/db/mongodb/helpers/mongo-helper.ts
@@ -0,0 +1,25 @@
+import { Collection, MongoClient } from 'mongodb'
+
+export const MongoHelper = {
+  client: null as unknown as MongoClient,
+
+  async connect(url: string): Promise<void> {
+    this.client = await MongoClient.connect(process.env.MONGO_URL as string, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    })
+  },
+
+  async disconnect(): Promise<void> {
+    await this.client.close()
+  },
+
+  getCollection(name: string): Collection {
+    return this.client.db().collection(name)
+  },
+
+  map: (collection: any): any => {
+    const { _id, ...collectionWithoutId } = collection
+    return Object.assign({}, collectionWithoutId, { id: _id })
+  }
+}
